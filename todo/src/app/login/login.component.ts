279905f2bb02463aa14eb855e4f82fa624cc556a
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 09:17:40 -0700

    implemented hardcoded-auth code and routers

--- todo/src/app/login/login.component.ts
+++ todo/src/app/login/login.component.ts
@@ -9,6 +9,8 @@ export class LoginComponent implements OnInit {
 
   username = 'Colin'
   password = ''
+  errorMessage = 'Invalid Credentials'
+  invalidLogin = false
 
   constructor() { }
 
@@ -16,8 +18,13 @@ export class LoginComponent implements OnInit {
   }
 
   handleLogin(){
-    console.log(this.username);
+    //console.log(this.username);
     //console.log(this.password);
+    if(this.username === 'Colin' && this.password === 'howie'){
+      this.invalidLogin = false
+    } else {
+      this.invalidLogin = true
+    }
   }
 
 }
