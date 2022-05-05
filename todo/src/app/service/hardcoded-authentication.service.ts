0ba521d3efbe2e7517ab88c95b99192cd17b27bc
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 16:17:25 -0700

    implemented hardcoded user authentication

---
+++ todo/src/app/service/hardcoded-authentication.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HardcodedAuthenticationService {
+  constructor() {}
+
+  authenticate(username: any, password: any) {
+    console.log('before: ' + this.isUserLoggedIn());
+    if (username === 'Colin' && password === 'howie') {
+      sessionStorage.setItem('authenticaterUser', username);
+      console.log('after: ' + this.isUserLoggedIn());
+      return true;
+    }
+    return false;
+  }
+
+  isUserLoggedIn() {
+    let user = sessionStorage.getItem('authenticaterUser');
+    return !(user === null);
+  }
+}
