00416f735735a906b84f8e813f4c46fa4cf5bf11
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 21:12:50 -0700

    first Hello World message from Spring Boot to Angular

--- todoservice/src/main/java/com/revature/todoservice/HelloWorldBean.java
+++ todoservice/src/main/java/com/revature/todoservice/HelloWorldBean.java
@@ -1,2 +1,23 @@
-package com.revature.todoservice;public class HelloWorldBean {
+package com.revature.todoservice;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("HelloWorldBean [message=%s]");
+    }
 }
--- todoservice/src/main/java/com/revature/todoservice/WelcomeController.java
+++ todoservice/src/main/java/com/revature/todoservice/WelcomeController.java
@@ -1,2 +1,20 @@
-package com.revature.todoservice;public class WelcomeController {
+package com.revature.todoservice;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+public class WelcomeController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World - Changed");
+    }
 }
