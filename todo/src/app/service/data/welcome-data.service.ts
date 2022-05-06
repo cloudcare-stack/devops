92e950d473a3bf37d5ce3bd66c710a0e2f772e73
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 22:59:50 -0700

    Call HTTP services with Path Variables and Error Handling

--- todo/src/app/service/data/welcome-data.service.ts
+++ todo/src/app/service/data/welcome-data.service.ts
@@ -17,4 +17,10 @@ export class WelcomeDataService {
     );
     //console.log('Execute Hello World Bean Service');
   }
+
+  executeHelloWorldServiceWithPathVariable(name: any) {
+    return this.http.get<HelloWorldBean>(
+      `http://localhost:8080/hello-world/path-variable/${name}`
+    );
+  }
 }
--- todo/src/app/welcome/welcome.component.ts
+++ todo/src/app/welcome/welcome.component.ts
@@ -31,12 +31,25 @@ export class WelcomeComponent implements OnInit {
   }
 
   getWelcomeMessage() {
-    console.log(this.service.executeHelloWorldBeanService());
-    this.service
-      .executeHelloWorldBeanService()
-      .subscribe((response) => this.handleSuccessfulResponse(response));
+    //console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldBeanService().subscribe(
+      (response) => this.handleSuccessfulResponse(response),
+      (error) => this.handleErrorResponse(error)
+    );
 
-    console.log('last line of getWelcomeMessage');
+    //console.log('last line of getWelcomeMessage');
+
+    //console.log('get welcome message');
+  }
+
+  getWelcomeMessageWithParameter() {
+    //console.log(this.service.executeHelloWorldBeanService());
+    this.service.executeHelloWorldServiceWithPathVariable(this.name).subscribe(
+      (response) => this.handleSuccessfulResponse(response),
+      (error) => this.handleErrorResponse(error)
+    );
+
+    //console.log('last line of getWelcomeMessage');
 
     //console.log('get welcome message');
   }
@@ -45,5 +58,12 @@ export class WelcomeComponent implements OnInit {
     this.welcomeMessageFromService = response.message;
     // console.log(response);
     // console.log(response.message);
+  }
+
+  handleErrorResponse(error: any) {
+    // console.log(error);
+    // console.log(error.error);
+    // console.log(error.error.message);
+    this.welcomeMessageFromService = error.error.message;
   }
 }
