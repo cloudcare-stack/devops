82bfcce677b8342da5d94ba700d08327b1d340c0
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 18:30:53 -0700

    add spring boot to run a RESTful web

---
+++ todoservice/src/test/java/com/revature/todoservice/TodoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.todoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
