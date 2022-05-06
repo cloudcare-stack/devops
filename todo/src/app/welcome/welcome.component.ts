f558748f7681963404be4f1ff456dbc55b2ff66f
cloudcare-stack <co8mcate@gmail.com>
2022-05-05 21:11:53 -0700

    first Hello World message from Spring Boot to Angular

--- todo/src/app/welcome/welcome.component.ts
+++ todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 //Java: @ComponentScan(value = "com.revature.springboot.web")
 @Component({
@@ -12,16 +13,37 @@ import { ActivatedRoute } from '@angular/router';
 export class WelcomeComponent implements OnInit {
   //String message = "Some welcome message"
   message = 'Some welcome message';
+  welcomeMessageFromService: string | undefined;
   name = '';
 
   //Java: public SpringBootFirstWebApplication(){}
   //Activated Router
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private service: WelcomeDataService
+  ) {}
 
   //Java: void init()
   ngOnInit(): void {
     console.log(this.message);
     // console.log(this.route.snapshot.params['name']);
     this.name = this.route.snapshot.params['name'];
+  }
+
+  getWelcomeMessage() {
+    console.log(this.service.executeHelloWorldBeanService());
+    this.service
+      .executeHelloWorldBeanService()
+      .subscribe((response) => this.handleSuccessfulResponse(response));
+
+    console.log('last line of getWelcomeMessage');
+
+    //console.log('get welcome message');
+  }
+
+  handleSuccessfulResponse(response: any) {
+    this.welcomeMessageFromService = response.message;
+    // console.log(response);
+    // console.log(response.message);
   }
 }
